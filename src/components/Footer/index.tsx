5aeba317e90e34352d55519d7e169beb9426857c
Lusca Lima <lusca.lima@outlook.com>
2022-06-16 22:57:16 -0300

    feat: add link to github

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,10 @@ import style from './style.module.scss'
 export default function Footer() {
   return (
     <footer className={style.footer}>
-      Made with 💙 by &nbsp; <a href="#">Luca Lima</a>
+      Made with 💙 by &nbsp;{' '}
+      <a href="https://github.com/LuscaLima" target="_blank">
+        Luca Lima
+      </a>
     </footer>
   )
 }
